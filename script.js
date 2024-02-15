cfec60de7a62f6ff27e2a8bd7f83ea0810e4c4a8
Am1d1v <dimadiman1adima@gmail.com>
2024-02-15 15:59:54 +0300

    Load Page Data

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
 
 
 const pagination = document.querySelector('.pages');
-const output = document.querySelector('output');
+const output = document.querySelector('.output');
+const posts = {
+    postsPerPage: 10,
+    currentPage: 1,
+    results: null
+}
 
 // Fetch Users Data
 const init = async function(){
@@ -10,7 +15,29 @@ const init = async function(){
 
     // Users JSON Format Data
     const formatedData = await dataRequest.json();
-    console.log(formatedData.results);
+
+    // All fetched results
+    posts.results = (formatedData.results);
+    loadPage(1)
+}
+
+// Load First page
+const loadPage = (page) => {
+    // Load First Page
+    posts.currentPage = page;
+
+    // Show Current Page
+    output.innerHTML = `<h1>Page: ${posts.currentPage}</h1>`;
+
+    // Load first 10 posts
+    for (let i = 0; i < posts.postsPerPage; i++) {
+        console.log(posts.results[i]);
+
+        // Show Loaded data in the DOM
+        const div = document.createElement('div');
+        div.textContent = `${posts.results[i].name.first}`;
+        output.appendChild(div);
+    }
 }
 
 window.addEventListener('load', () => {
