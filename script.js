00304f0a430f2e0cdafa420dc3b12df32f07dd79
Am1d1v <dimadiman1adima@gmail.com>
2024-02-15 16:25:44 +0300

    Load Page Content

--- script.js
+++ script.js
@@ -18,24 +18,35 @@ const init = async function(){
 
     // All fetched results
     posts.results = (formatedData.results);
-    loadPage(1)
+    loadPage(9)
 }
 
 // Load First page
 const loadPage = (page) => {
-    // Load First Page
+
+    // Current Page
     posts.currentPage = page;
+
+    // Page Posts
+    let startPost = (posts.currentPage - 1) * posts.postsPerPage;
+
+    //
+    let endPost = startPost + posts.postsPerPage > posts.results.length ? posts.results.length : startPost + posts.postsPerPage;
+
+    // Total Pages
+    let totalPages = Math.ceil(posts.results.length / posts.postsPerPage);
+    console.log(startPost, endPost)
 
     // Show Current Page
     output.innerHTML = `<h1>Page: ${posts.currentPage}</h1>`;
 
     // Load first 10 posts
-    for (let i = 0; i < posts.postsPerPage; i++) {
+    for (let i = startPost; i < endPost; i++) {
         console.log(posts.results[i]);
 
         // Show Loaded data in the DOM
         const div = document.createElement('div');
-        div.textContent = `${posts.results[i].name.first}`;
+        div.textContent = `${i} ${posts.results[i].name.first}`;
         output.appendChild(div);
     }
 }
